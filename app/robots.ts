21529be4a89837e151068d708ff057ad72eafa0e
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-09 01:01:15 +0700

    feat: update robots.txt rules for better bot handling

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,27 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+      {
+        userAgent: [
+          "Twitterbot",
+          "facebookexternalhit",
+          "facebot",
+          "Slackbot",
+          "Discordbot",
+          "WhatsApp",
+          "WhatsAppBot",
+          "vercel-og",
+          "vercel-favicon",
+          "vercel-screenshot",
+          "LinkedInBot",
+        ],
+        disallow: [" "],
+      },
+    ],
   };
 }
