72a9b9b2233a4e6448360934c7238ee1e5a09414
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-30 01:52:15 +0700

    chore: allow all user agent

--- app/robots.ts
+++ app/robots.ts
@@ -2,27 +2,9 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        disallow: "/",
-      },
-      {
-        userAgent: [
-          "Twitterbot",
-          "facebookexternalhit",
-          "facebot",
-          "Slackbot",
-          "Discordbot",
-          "WhatsApp",
-          "WhatsAppBot",
-          "vercel-og",
-          "vercel-favicon",
-          "vercel-screenshot",
-          "LinkedInBot",
-        ],
-        disallow: [" "],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
   };
 }
